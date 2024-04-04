21dbf54415818d0035d85b1a6003ba0c18865a6d
Dee Jae Mann <dee.j.mann@hotmail.com>
2024-04-04 17:30:21 -0400

    Django: Added auth config and user_app

--- back-end/careercompass_api/settings.py
+++ back-end/careercompass_api/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "rest_framework.authtoken",
+    "user_app",
 ]
 
 MIDDLEWARE = [
@@ -103,6 +105,12 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+# Token Authentication
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication'
+    ],
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
@@ -125,3 +133,6 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Specify user model
+AUTH_USER_MODEL = "user_app.CCUser"
