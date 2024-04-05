739d1795868bdbfa14e7befe8da5a72ffab98161
Dee Jae Mann <dee.j.mann@hotmail.com>
2024-04-05 17:58:34 -0400

    Added validator to email

--- back-end/user_app/models.py
+++ back-end/user_app/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.core import validators as v
+from .validators import validate_email
 
 # Create your models here.
 class CCUser(AbstractUser):
-    email = models.EmailField(unique=True)
+    email = models.EmailField(unique=True, validators=[validate_email])
     USERNAME_FIELD='email'
     REQUIRED_FIELDS=[]
