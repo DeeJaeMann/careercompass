21bc163417752dd037cd6ccbc7f57ce2682f4dc3
Dee Jae Mann <dee.j.mann@hotmail.com>
2024-04-06 18:00:33 -0400

    Added login endpoint

--- back-end/user_app/urls.py
+++ back-end/user_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import SignUp
+from .views import SignUp, LogIn
 
 urlpatterns = [
     path("signup/", SignUp.as_view(), name="sign-up"),
+    path("login/", LogIn.as_view(), name="login"),
 ]
