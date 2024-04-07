ed6092fb301e7593da8410f0b949471aa00dec9d
Dee Jae Mann <dee.j.mann@hotmail.com>
2024-04-07 11:12:59 -0400

    Added test comments and test_008

--- back-end/tests/test_models.py
+++ back-end/tests/test_models.py
@@ -2,10 +2,18 @@ from django.test import TestCase
 from django.core.exceptions import ValidationError
 from django.db import IntegrityError, DataError
 from user_app.models import CCUser
+from keyword_app.models import Keyword
+
 
 # Part I: User Model
 class TestCCUser(TestCase) :
+    """
+    CCUser Model Tests
+    """
     def test_001_user_with_proper_email_field(self):
+        """
+        This test will create a user with proper fields
+        """
         new_ccuser = CCUser.objects.create(
             email="me@here.com",
             username="me@here.com",
@@ -16,6 +24,9 @@ class TestCCUser(TestCase) :
         self.assertIsNotNone(new_ccuser)
 
     def test_002_user_with_improper_email_field(self):
+        """
+        This test will attempt to create a user with improper email field
+        """
         try:
             new_ccuser = CCUser.objects.create(
                 email="me",
@@ -27,3 +38,26 @@ class TestCCUser(TestCase) :
             self.fail()
         except ValidationError as error:
             self.assertTrue("email" in error.message_dict)
+
+class TestKeyword(TestCase):
+    """
+    Keyword Model Tests
+    """
+    def setUp(self):
+        CCUser.objects.create(
+            email="me@here.com",
+            username="me@here.com",
+            password="1234",
+        )
+
+    def test_008_keyword_with_proper_fields(self):
+        ccuser = CCUser.objects.get(username="me@here.com")
+
+        new_keyword = Keyword.objects.create(
+            category="interest",
+            name="history",
+            user=ccuser
+        )
+
+        new_keyword.full_clean()
+        self.assertIsNotNone(new_keyword)
