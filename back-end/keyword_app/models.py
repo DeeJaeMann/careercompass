ec35952f45b368bce130c1da0746ef9fbfe2a7f8
Dee Jae Mann <dee.j.mann@hotmail.com>
2024-04-07 10:40:20 -0400

    Created Keyword Model

--- back-end/keyword_app/models.py
+++ back-end/keyword_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.utils import timezone
+from django.core import validators as v
+from user_app.models import CCUser
 
 # Create your models here.
+class Keyword(models.Model):
+    category = models.CharField()
+    name = models.CharField()
+    date_added = models.DateTimeField(default=timezone.now)
+    user = models.ForeignKey(CCUser, on_delete=models.CASCADE, related_name='keywords')
