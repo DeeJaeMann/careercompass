d8fbd08031b3803b3440f99bebbe9401b81dbe4f
Dee Jae Mann <dee.j.mann@hotmail.com>
2024-04-08 12:51:09 -0400

    Created view for all of user keywords

--- back-end/keyword_app/urls.py
+++ back-end/keyword_app/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import CreateKeyword, KeywordInfo
+from .views import CreateKeyword, KeywordInfo, KeywordAllInfo
 
 urlpatterns = [
-    path("create/", CreateKeyword.as_view(), name="create-keyword"),
+    path("", KeywordAllInfo.as_view(), name="user-keywords"),
     path("<int:id>/", KeywordInfo.as_view(), name="get-keyword"),
+    path("create/", CreateKeyword.as_view(), name="create-keyword"),
 ]
