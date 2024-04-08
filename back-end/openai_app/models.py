4a1660b18277f952ea559e96d0ad228af110985c
Dee Jae Mann <dee.j.mann@hotmail.com>
2024-04-08 15:12:40 -0400

    Added app, model and validators.

--- back-end/openai_app/models.py
+++ back-end/openai_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.utils import timezone
+from user_app.models import CCUser
+from .validators import validate_onet_code
 
 # Create your models here.
+class Occupation(models.Model):
+    name= models.CharField(blank=False, null=False, max_length=150)
+    onet_code = models.CharField(blank=False, null=False, max_length=10, validators=[validate_onet_code])
+    date_added = models.DateTimeField(default=timezone.now)
+    user = models.ForeignKey(CCUser, on_delete=models.CASCADE, related_name="occupations")
+
+    class Meta:
+        unique_together = ('onet_code','user')
---
+++ back-end/openai_app/validators.py
@@ -0,0 +1,14 @@
+from django.core.exceptions import ValidationError
+import re
+from lib.logger import warn_logger
+
+def validate_onet_code(onet_code):
+    error_message = 'Onet_Code: Must be in this format ##-####.##'
+
+    code_pattern = r'^\d{2}-\d{4}\.\d{2}$'
+    valid_code = re.match(code_pattern, onet_code)
+    if valid_code:
+        return onet_code
+    
+    warn_logger.warning(f"Occupation: Onet Code validation failed: {onet_code}")
+    raise ValidationError(error_message, params={'Current Value':onet_code})
