f3611d3c7e8b4b4de2832d09d655b8ea4c55034d
Dee Jae Mann <dee.j.mann@hotmail.com>
2024-04-11 12:17:46 -0400

    Added note to describe correct ID for endpoint

--- back-end/onet_app/urls.py
+++ back-end/onet_app/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import DetailsInfo
 
 urlpatterns = [
+    #NOTE This ID is the Occupation ID, not the Details ID
     path("<int:id>/", DetailsInfo.as_view(), name="occupation-details"),
 ]
