ac0d89251786f5078f20aa21768223ac8731ffea
Dee J. Mann <dee.j.mann@hotmail.com>
2024-04-16 19:43:12 -0400

    Added footer Moved ONet credit to footer 	modified:   App.jsx 	new file:   components/Footer.jsx 	modified:   pages/HomePage.jsx

---
+++ front-end/src/components/Footer.jsx
@@ -0,0 +1,19 @@
+
+const Footer = () => {
+
+    return (
+        <>
+            <div className="d-flex justify-center mt-5">
+                <span>
+                    <a href="https://services.onetcenter.org/" title="This site incorporates information from O*NET Web Services. Click to learn more." target="_blank"><img src="https://www.onetcenter.org/image/link/onet-in-it.svg" alt="O*NET in-it" /></a>
+                </span>
+                <span className="align-bottom">
+                    <p className="text-xs">This site incorporates information from <a href="https://services.onetcenter.org/">O*NET Web Services</a> by the U.S. Department of Labor, Employment and Training Administration (USDOL/ETA). O*NET&reg; is a trademark of USDOL/ETA.</p>
+                </span>
+            </div>
+        </>
+    )
+
+}
+
+export default Footer
