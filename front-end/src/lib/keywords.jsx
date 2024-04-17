5ca38f9fffbb329951ba31cdd307451d7f4ce1f9
Dee J. Mann <dee.j.mann@hotmail.com>
2024-04-17 14:36:25 -0400

    Added error logging to keyword_app - modify keyword Added Create and Update logic to keywords lib file Added Conditional rendering for keyword list to populate existing keywords or display fields to enter new ones Changes to be committed: 	modified:   back-end/keyword_app/views.py 	modified:   back-end/log/error.log 	modified:   back-end/log/info.log 	modified:   front-end/src/lib/keywords.jsx 	modified:   front-end/src/pages/KeywordsPage.jsx

--- front-end/src/lib/keywords.jsx
+++ front-end/src/lib/keywords.jsx
@@ -1,30 +1,89 @@
 import { ccAPI } from "./connections";
 
 export const getKeywords = async() => {
+    // Gets the stored keywords from the API
     try {
         const response = await ccAPI.get("keyword/")
 
-        if (response.status === 200) {
-
-        console.log(`Keyword Connection ${await response.status} ${await response.data} ${await response}`)
-        console.log(await response.data)
-
-        return await response.data;
-        }
+        if (response.status === 200) return await response.data;
         alert(response.data);
-        console.log(`Error: ${response.data}`)
-        return []
+        console.log(`getKeywords Error: ${response.data}`)
+        return null
     }
     catch (error) {
 
-        if(response.status === 401) {
-            console.log(`User not logged in!`);
+        if(error.response.status === 401) {
             alert(`You need to login first!`)
-            return []
+            return null
         }
 
-        console.log(`Error ${error} ${response.data}`)
-        alert(`${error}`)
-        return []
+        if(error.response.status === 400){
+            console.log(`createKeyword error: ${Object.keys(error.response.data).map((err) => `${err} - ${error.response.data[err]}`)}`)
+            alert(`Invalid data: ${Object.keys(error.response.data).map((err) => `${err} - ${error.response.data[err]}`)}`)
+            return null
+        }
+        console.log(`createKeyword error: ${Object.keys(error.response.data).map((err) => `${err} - ${error.response.data[err]}`)}`)
+        alert(`Error: ${Object.keys(error.response.data).map((err) => `${err} - ${error.response.data[err]}`)}`)
+        return null
+    }
+}
+
+export const createKeyword = async(this_keyword) => {
+    // Creates a new keyword in the API
+    try {
+    const response = await ccAPI.post("keyword/create/", this_keyword)
+
+        if(response.status === 201) {
+
+            console.log(`createKeyword Response: ${response}`)
+            return await response.data
+        }
+        alert(response.data)
+        console.log(`createKeyword Error: ${response.data} - ${response}`)
+
+    }
+    catch (error){
+        if(error.response.status === 401) {
+            alert(`You need to login first!`)
+            return null
+        }
+        if(error.response.status === 400){
+            console.log(`createKeyword error: ${Object.keys(error.response.data).map((err) => `${err} - ${error.response.data[err]}`)}`)
+            alert(`Invalid data: ${Object.keys(error.response.data).map((err) => `${err} - ${error.response.data[err]}`)}`)
+            return null
+        }
+        console.log(`createKeyword error: ${Object.keys(error.response.data).map((err) => `${err} - ${error.response.data[err]}`)}`)
+        alert(`Error: ${Object.keys(error.response.data).map((err) => `${err} - ${error.response.data[err]}`)}`)
+        return null
+    }
+}
+
+export const updateKeyword = async(this_keyword) => {
+    // Updates an existing keyword with the API
+    try {
+
+        const response = await ccAPI.put(`keyword/${this_keyword.id}/`, this_keyword)
+
+        if(response.status === 201) {
+            console.log(`updateKeyword Response: ${response.data}`)
+            return await response.data
+        }
+        alert(response.data)
+        console.log(`updateKeyword Error ${response.data} ${response}`)
+        return response
+    }
+    catch (error){
+        if(error.response.status === 401) {
+            alert(`You need to login first!`)
+            return null
+        }
+        if(error.response.status === 400){
+            console.log(`updateKeyword error: ${Object.keys(error.response.data).map((err) => `${err} - ${error.response.data[err]}`)}`)
+            alert(`Invalid data: ${Object.keys(error.response.data).map((err) => `${err} - ${error.response.data[err]}`)}`)
+            return null
+        }
+        console.log(`updateKeyword error: ${Object.keys(error.response.data).map((err) => `${err} - ${error.response.data[err]}`)}`)
+        alert(`Error: ${Object.keys(error.response.data).map((err) => `${err} - ${error.response.data[err]}`)}`)
+        return null
     }
 }
--- front-end/src/pages/KeywordsPage.jsx
+++ front-end/src/pages/KeywordsPage.jsx
@@ -1,11 +1,16 @@
 import { useState, useEffect } from "react";
 import { useOutletContext } from "react-router-dom";
 import { MDBBtn } from 'mdb-react-ui-kit';
-import { getKeywords } from "../lib/keywords";
+import { 
+    getKeywords,
+    createKeyword,
+    updateKeyword,
+} from "../lib/keywords";
 
 const KeywordsPage = () => {
 
     const [ keywords, setKeywords ] = useState([])
+    const [ keywordUpdate, setKeywordUpdate] = useState(false)
     const { user } = useOutletContext();
 
     useEffect(() => {
@@ -18,32 +23,93 @@ const KeywordsPage = () => {
                 return catA.localeCompare(catB);
             });
             setKeywords(response)
-            console.log(`Keywords: ${response}`)
+            setKeywordUpdate(false)
+            // console.log(`Keywords: ${response}`)
         }).catch(error => {
-            console.log(`Keywords useEffect Error ${error} - ${resposne}`)
+            console.log(`Keywords useEffect Error ${error} - ${response}`)
         })
-    }, [])
+    }, [keywordUpdate])
 
-    const handleSubmit = (event, keyword) => {
+    const handleUpdate = (event, this_keyword) => {
         event.preventDefault()
 
-        alert(`Submit! ID: ${keyword.id} ${keyword.name}`)
+        if(this_keyword.name) {
+            updateKeyword(this_keyword).then(response => {
+                if(response) {
+                    console.log(`Updated: ${response.id} ${response.name}`)
+                    alert(`Updated: ${response.name}`)
+                    setKeywordUpdate(true)
+                }
+            })
+        }
+        else {
+            alert(`You must enter a value!`)
+        }
+
+    }
+
+    const handleCreate = async(event, this_keyword) => {
+        event.preventDefault()
+
+        if(this_keyword.name) {
+            createKeyword(this_keyword).then(response => {
+                if(response){
+                    console.log(`Created: ${response.id} ${response.name}`)
+                    alert(`Created: ${response.name}`)
+                    setKeywordUpdate(true)
+                }
+            })
+        }
+        else {
+            alert(`You must enter a value!`)
+        }
+    }
+
+    const displayCreateForm = (this_keyword, index) => {
+        return (
+            <>
+                <form key={index} onSubmit={(event) => handleCreate(event, this_keyword)}>
+                    <span>{this_keyword.category}</span>
+                    <input className="ml-2 mb-1 rounded-md border-slate-900 border" type="text" placeholder={`Enter ${this_keyword.category}`} defaultValue={this_keyword.name} onChange={(event) => this_keyword.name = event.target.value} />
+                    <MDBBtn className="bg-blue-900 ml-2 mb-1 h-7 p-2 pt-1 pb-1">Submit</MDBBtn>
+                </form>
+            </>
+        )
+    }
+
+    const displayCreateFields = () => {
+        let fieldOutput = [];
+
+        for( let index = 0; index < 6; index++) {
+            const this_keyword = {}
+            index < 3 ? (
+                this_keyword.category = "hobby"
+            ) : (
+                this_keyword.category = "interest"
+            )
+            if(keywords[index]) {
+                this_keyword.name = keywords[index].name
+            }
+            fieldOutput.push(displayCreateForm(this_keyword, index))
+        }
+        return fieldOutput
     }
 
     return (
         <>
             <h2>Keywords Page</h2>
             <p>{user && `${user}`}</p>
-                {!keywords.length ? (
+                {keywords.length < 6 ? (
                     <>
                         <p>No Keywords</p>
+                        {displayCreateFields()}
                     </>
                 ) : (
                 <>
-                {keywords.map((keyword, index) => 
-                    <form key={index} onSubmit={(event) => handleSubmit(event, keyword)}> 
-                        <span>{keyword.category}</span>
-                        <input className="ml-2 mb-1 rounded-md border-slate-900 border" type="text" name={`${keyword.category}${keyword.id}`} placeholder={`Enter ${keyword.category}`} defaultValue={keyword.name} onChange={(event) => keyword.name = event.target.value} />
+                {keywords.map((this_keyword, index) => 
+                    <form key={index} onSubmit={(event) => handleUpdate(event, this_keyword)}> 
+                        <span>{this_keyword.category}</span>
+                        <input className="ml-2 mb-1 rounded-md border-slate-900 border" type="text" name={`${this_keyword.category}${this_keyword.id}`} placeholder={`Enter ${this_keyword.category}`} defaultValue={this_keyword.name} onChange={(event) => this_keyword.name = event.target.value} />
                         <MDBBtn className="bg-blue-900 ml-2 mb-1 h-7 p-2 pt-1 pb-1">Submit</MDBBtn>
                     </form>
                 )}
