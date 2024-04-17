8f4d1016d39e82ae6ad306ee5d4d953775a09fdc
Dee J. Mann <dee.j.mann@hotmail.com>
2024-04-17 19:51:52 -0400

    Added listing for occupations Occupation details not functional updated occupations lib file Changes to be committed: 	modified:   back-end/log/info.log 	modified:   front-end/src/lib/occupations.jsx 	modified:   front-end/src/pages/OccupationsPage.jsx

--- front-end/src/lib/occupations.jsx
+++ front-end/src/lib/occupations.jsx
@@ -7,34 +7,44 @@ export const getOccupations = async() => {
         // Check for 6 keywords first
         const keyword_response = await getKeywords();
 
-        // if (keyword_response.status === 200 || keyword_response.status === 201) {
-        console.log(`getOccupation loaded keywords`)
         if(keyword_response.length === 6) {
-            console.log(`getOccupation has enough keywords`)
-            const response = await ccAPI.get("occupation/")
+            const response = await ccAPI.get("occupation/");
             return await response.data;
         }
         else {
-            console.log(`Not enough keywords: counted ${keyword_response.length}`)
-            alert(`You must have 3 hobbies and 3 interests!`)
-            return null
+            console.log(`Not enough keywords: counted ${keyword_response.length}`);
+            alert(`You must have 3 hobbies and 3 interests!`);
+            return null;
         }
-        // }
-        console.log(`getOccupations keyword ${keyword_response}`)
-        console.log(`getOccupations Error: ${keyword_response.data}`)
-        console.log(`getOccupations status ${await keyword_response.status}`)
-        alert(`getOccupations Error: ${keyword_response.data}`);
-        return null;
     }
     catch (error) {
-        console.log(`getOccupation catch ${error}`)
+        console.log(`getOccupation catch ${error}`);
 
         if(error.response.status === 401) {
-            alert(`You need to login first!`)
-            return null
+            alert(`You need to login first!`);
+            return null;
         }
 
-        console.log(`getOccupations error: ${Object.keys(error.response.data).map((err) => `${err} - ${error.response.data[err]}`)}`)
+        console.log(`getOccupations error: ${Object.keys(error.response.data).map((err) => `${err} - ${error.response.data[err]}`)}`);
+        alert(`Error: ${Object.keys(error.response.data).map((err) => `${err} - ${error.response.data[err]}`)}`);
+        return null;
+    }
+}
+
+export const getDetails = async(id) => {
+    try {
+        const response = await ccAPI.get(`details/${id}/`);
+
+        // console.log(`getDetails ${Object.keys(response.data)}`)
+        return response.data
+    }
+    catch (error) {
+        if(error.response.status === 404) {
+            console.log(`getDetails catch ${error} ID: ${id}`)
+            alert(`Error ${error}`)
+            return null
+        }
+        console.log(`getDetails error: ${Object.keys(error.response.data).map((err) => `${err} - ${error.response.data[err]}`)}`)
         alert(`Error: ${Object.keys(error.response.data).map((err) => `${err} - ${error.response.data[err]}`)}`)
         return null
     }
--- front-end/src/pages/OccupationsPage.jsx
+++ front-end/src/pages/OccupationsPage.jsx
@@ -1,10 +1,11 @@
 import { useState, useEffect } from "react";
 import { useOutletContext } from "react-router-dom";
-import { getOccupations } from "../lib/occupations";
+import { getOccupations, getDetails } from "../lib/occupations";
 
 const OccupationsPage = () => {
     const { user } = useOutletContext();
     const [ occupations, setOccupations ] = useState([])
+    const [ thisDetail, setThisDetail ] = useState([])
 
     useEffect(() => {
         getOccupations().then(response => {
@@ -14,7 +15,22 @@ const OccupationsPage = () => {
                     const nameB = b.name.toUpperCase();
                     return nameA.localeCompare(nameB);
                 });
-
+                // for(let job of response){
+                //     getDetails(job.id).then(thisResponse => setThisDetail([...thisDetail, thisResponse]))
+                //     console.log(`Job: ${job.id}`)
+                //     console.log(`Detail Response: ${thisDetail}`)
+                    // job.details = thisDetail.description
+                // }
+                // response = response.map((job) => {
+                //     getDetails(job.id).then(detail_response => {
+                //         job['details'] = detail_response.description
+                //         job['alt_names'] = detail_response.alt_names
+                //         job['tasks'] = detail_response.tasks
+                //         job['id'] = job.id
+                //         job['name'] = job.name
+                //     })
+                // console.log(`Response object keys: ${Object.keys(response)}`)
+                // })
                 setOccupations(response)
             }
         }).catch(error => {
@@ -22,9 +38,10 @@ const OccupationsPage = () => {
         })
     }, [])
 
+
     return (
         <>
-            <h2>Occupations Page</h2>
+            <h2>Here is what I found for you:</h2>
             <ol>
                 {occupations.map((job, index) =>
                 <li key={index}>{job.name}</li>
